5b062794e8e838cdbd7c9005d11346ad73fb4560
SlotGacor2026 <fandieko758@gmail.com>
2026-02-03 09:42:49 +0700

    Update data-games.js

--- js/data-games.js
+++ js/data-games.js
@@ -1,9 +1,8 @@
-const GAMES=[];
-for(let i=1;i<=220;i++){
-  GAMES.push({
-    id:i,
-    name:`Slot ${i}`,
-    provider:i%2===0?'PGSoft':'Pragmatic',
-    img:`slot${i}.jpg`
-  });
-    }
+// Daftar semua game slot
+const GAMES = [
+  { id: 1, name: "Mahjong Ways 1", provider: "PGSoft", img: "mahjong_ways_1.png", file: "mahjong_ways_1.html" },
+  { id: 2, name: "Mahjong Ways 2", provider: "PGSoft", img: "mahjong_ways_2.png", file: "mahjong_ways_2.html" },
+  { id: 3, name: "Mahjong Ways 3", provider: "PGSoft", img: "mahjong_ways_3.png", file: "mahjong_ways_3.html" },
+  { id: 4, name: "Wild Bounty", provider: "Pragmatic", img: "wild_bounty.png", file: "wild_bounty.html" },
+  { id: 5, name: "Gates of Olympus", provider: "Pragmatic", img: "gates_of_olympus.png", file: "gates_of_olympus.html" }
+];
